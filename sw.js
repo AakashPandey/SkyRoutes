4ad59357bbad591bc310b3a0ff4c65c783213f12
Aakash Pandey <Aakash.pandey@live.com>
2019-01-25 23:48:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,73 +1,12 @@
-var connectDB = (dbDef, resolve, reject) => {
-    return new Promise((resolve, reject) => {
-        // Opens a connection to the existing database or creates a new one    
-        req = indexedDB.open(dbDef.dbName, dbDef.dbVer);
-        req.onsuccess = (ev) => {
-            // Saves an instance of the connection to our custom object        
-            dbDef.dbCon = ev.target.result;
-            resolve();
-        }
-        req.onupgradeneeded = (event) => {
-            // Only fired once per db version, used to initiliaze the db
-            dbDef.dbCon = event.target.result;
-            dbDef.dbInit = 1;
-            resolve();
-        }
-        req.onerror = (e) => {
-            // Returns error event
-            reject(e);
-        }
-    });
-  }
+const chng = ` Tinker with this string in order to push updates to your app. `;
 
-var readDB = (dbDef, key, dex) => {
-    return new Promise((resolve, reject) => {
-        var trx = dbDef.dbCon.transaction([dbDef.dbStore]).objectStore(dbDef.dbStore);
-        // If reading by an index
-        if (dex) {
-            // request index lookup
-            trx = trx.index(dex);
-            trx.get(key).onsuccess = (r) => {
-                if (r.target.result !== undefined) {
-                    resolve(r.target.result);
-                } else {
-                    resolve(`[readDB] ${dbDef.dbStore}, key: ${key} not found (${dex})`)
-                }
-            }
-        }
-        trx = trx.get(key);
-        trx.onsuccess = (r) => {
-            if (r.target.result === undefined) {
-                reject(`[readDB] ${dbDef.dbStore}, key: ${key} not found`);
-            } else {
-                resolve(r.target.result);
-            }
-        }
-        trx.onerror = (e) => {
-            reject(e);
-        }
-    });
-  }
-
-  
-var dbDef = {
-    dbName: "skyroutes",
-    dbVer: 1,
-    dbStore: "routes",
-    dbKeyp: "auto",
-};
-
-console.log("[SKYROUTES-BETA-0.6] An innovative client side router by Aakash Pandey");
-const chng = ["initial testing"];
-const vn = "version-p10";
+const vn = "version-s1";
 
 var appCash = [
     '/index.html',
     '/lib/skyroute.css',
     '/lib/skyroute.js'
 ];
-
-var RoutesSW;
 
 self.addEventListener('install', (e) => {
     e.waitUntil(
@@ -77,43 +16,20 @@ self.addEventListener('install', (e) => {
     );
 });
 
-
-self.addEventListener('message', (event) => {
-    if (event.data.action === 'skipWaiting') {
-        self.skipWaiting();
-    } else if (event.data.action === 'clearOld') {
-        event.waitUntil(
-            caches.keys().then((keys) => Promise.all(
-                keys.map((k) => {
-                    if (!vn.includes(k)) {
-                        return caches.delete(k);
-                    }
-                })
-            )).then(() => {
-                console.log('[App Update]');
-            })
-        )
-    }
-});
-
-// RoutesSW[url.pathname.slice(1,)]
-
-
 self.addEventListener('fetch', e => {
     const url = new URL(e.request.url);
-    connectDB(dbDef).then((c)=>{
-        readDB(dbDef, 1).then((r)=>{
-            RoutesSW = r;
-            // console.log(RoutesSW);
-        });
-    });
-    if (url.origin === location.origin && (!url.pathname.includes(".")) && (RoutesSW[url.pathname.slice(1,)])) {
-        var r = caches.match(location.origin + '/index.html');
-        e.respondWith(r)
-        
-    } else if(appCash.includes(url.pathname)) {
-        e.respondWith(caches.match(url))
+    // e.waitUntil(pauseReq);
+    if (e.request.mode === "navigate" && url.origin === location.origin && (!url.pathname.includes("."))) {
+        e.respondWith(pauseReq(url));
     } else {
-        // console.log("Server Fetch: " + url);
+        (appCash.includes(url.pathname)) ? e.respondWith(caches.match(url)) : e.respondWith(fetch(url));
     }
 });
+
+
+var readDB=(dbDef,key,dex)=>{return new Promise((resolve,reject)=>{req=indexedDB.open(dbDef.dbName,dbDef.dbVer);req.onsuccess=(ev)=>{req=ev.target.result;var trx=req.transaction([dbDef.dbStore]).objectStore(dbDef.dbStore);trx=trx.get(key);trx.onsuccess=(r)=>{routesSW=r.target.result;resolve()}
+trx.onerror=(e)=>{reject(e)}}})}
+var routesSW=0;self.addEventListener('message',(event)=>{if(event.data.action==='skipWaiting'){self.skipWaiting()}else if(event.data.action==='clearOld'){event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.map((k)=>{if(!vn.includes(k)){return caches.delete(k)}}))).then(()=>{console.log('[App Updated]')}))}else if(event.data.action==='rescanPath'){console.log("rebuilding paths");dbScan()}});var dbScan=()=>{return new Promise((r)=>{
+readDB(dbDef,1).then(e=>{r()})})}
+var pauseReq=async(url)=>{if(routesSW===0){await dbScan()}
+if(routesSW[url.pathname.slice(1)]){return caches.match("/index.html")}else{return fetch(url)}};var dbDef={dbName:"SkyRoutes",dbVer:1,dbStore:"routes"}
